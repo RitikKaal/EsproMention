8ebef8259a4e76a76fe7d38033dabef1cbdff8e8
RitikKaal <157566142+RitikKaal@users.noreply.github.com>
2024-03-01 08:11:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-hb
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
